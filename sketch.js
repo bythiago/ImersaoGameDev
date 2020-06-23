da16b5e60688b2463c91549909fa5e865c4a7f05
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-22 22:23:31 -0300

    fix - criando cenario e adicionando personagem

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let scenery;
+let character;
+
+let altura = 135;
+
+function preload(){
+  scenery = loadImage('https://raw.githubusercontent.com/bythiago/ImersaoGameDev/master/assets/imagens/cenario/floresta.png');
+  character = loadImage('https://raw.githubusercontent.com/bythiago/ImersaoGameDev/master/assets/imagens/personagem/correndo.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(scenery);
+  image(character, 0, height - altura, 110, altura, 0, 0, 220, 270);
 }
