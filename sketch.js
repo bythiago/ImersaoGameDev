87156db62c0ad999c351d48253887a40f93da7e4
Thiago Vieira <thiiagovieir@gmail.com>
2020-06-22 22:49:04 -0300

    add - criando classe para manipular o cenario

--- sketch.js
+++ sketch.js
@@ -1,18 +1,33 @@
-let scenery;
+let sceneryImage;
 let character;
-
 let altura = 135;
+let scenery;
+
 
 function preload(){
-  scenery = loadImage('https://raw.githubusercontent.com/bythiago/ImersaoGameDev/master/assets/imagens/cenario/floresta.png');
+  sceneryImage = loadImage('https://raw.githubusercontent.com/bythiago/ImersaoGameDev/master/assets/imagens/cenario/floresta.png');
   character = loadImage('https://raw.githubusercontent.com/bythiago/ImersaoGameDev/master/assets/imagens/personagem/correndo.png');
 }
 
 function setup() {
+  scenery = new Scenery(sceneryImage, 50);
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background(scenery);
+function draw(){
+  scenery.display();
   image(character, 0, height - altura, 110, altura, 0, 0, 220, 270);
+}
+
+
+class Scenery {
+  constructor(image, velocity){
+    this.image = image;
+    this.velocity = velocity;
+  }
+
+  display(){
+    image(this.image, -this.velocity, 0, width, height);
+    image(this.image, width -this.velocity, 0, width, height);
+  }
 }
